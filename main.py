1d41b71a676caa12055daa64f43549861414c115
Tryfolos <70145934+Tryfolos@users.noreply.github.com>
2020-11-15 22:25:35 +0100

    done

--- main.py
+++ main.py
@@ -11,10 +11,17 @@ for x in range(1, number_of_rows + 1):
 	if number_of_rows >= x:
 		while number_of_rows >= position:
 			thing = startnumber * multiplier
-			if thing >= 10:
+			if thing >= 1000 and thing < 10000:
+				print(f"{thing} ", end = "")
+			if thing >= 100 and thing < 1000:
+				print(f"{thing}  ", end = "")
+			if thing >= 10 and thing < 100:
 				print(f"{thing}   ", end = "")
 			if thing < 10:
 				print(f"{thing}    ", end = "")
+			if thing >= 10000:
+				print("NUMBER IS TOO BIG")
+				break
 			multiplier += 1
 			position += 1
 		else:
@@ -22,7 +29,4 @@ for x in range(1, number_of_rows + 1):
 			startnumber += 1
 			multiplier = 1
 			position = 1
-
-
-
 
